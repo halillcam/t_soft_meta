ce76a0362f4da583783ef31d7fa744bec4220c6d
natanz <halil.cam0020@gmail.com>
2025-09-24 00:03:04 +0300

    proje dosyası oluşturduldu gerekli klasorleme yapısı yapıldı ve projeye model oluşturularak başlandı

---
+++ android/app/src/main/kotlin/com/example/t_soft_meta/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.t_soft_meta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/model/item_model.dart
@@ -0,0 +1,11 @@
+class ItemModel {
+  String productName;
+  int price;
+  List<String> shops;
+
+  ItemModel({
+    required this.productName,
+    required this.price,
+    required this.shops,
+  });
+}
